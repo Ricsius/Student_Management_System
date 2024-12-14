564a341389aca0eec9c368ed309fe8dd33399521
Ricsius <hajnal.ricsi@gmail.com>
2024-12-15 00:20:07 +0100

    Add about dialog

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ class MainWindow(QMainWindow):
 
         help_menu_item = self.menuBar().addMenu("&Help")
         about_action = QAction("About", self)
+        about_action.triggered.connect(self.about)
         help_menu_item.addAction(about_action)
 
         edit_menu_item = self.menuBar().addMenu("&Edit")
@@ -128,6 +129,10 @@ class MainWindow(QMainWindow):
 
     def delete(self):
         dialog = DeleteDialog()
+        dialog.exec()
+
+    def about(self):
+        dialog = AboutDialog()
         dialog.exec()
 
 class InsertDialog(QDialog):
@@ -282,6 +287,18 @@ class DeleteDialog(QDialog):
         confirmation_widget.setWindowTitle("Success")
         confirmation_widget.setText("The record was deleted successfully!")
         confirmation_widget.exec()
+
+class AboutDialog(QMessageBox):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("About")
+
+        content = """
+        This app was created during the course "The Python Mega Course".
+        Fell free to modify and reuse this app.
+        """
+
+        self.setText(content)
 
 app = QApplication(sys.argv)
 main_window = MainWindow()
